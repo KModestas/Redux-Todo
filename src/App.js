f7b28056f8f3914ab753166314c9a4cf8ae57837
Modestas <modestask97@gmail.com>
2018-04-17 12:45:45 +0100

    reset list component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import AddTodo from './components/AddTodo';
 import List from './components/List';
+import ResetList from './components/ResetList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AddTodo />
+        <ResetList />
         <List />
       </div>
     );
--- src/components/ResetList.js
+++ src/components/ResetList.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { resetList } from '../actions';
+
+
+const ResetList = () => (
+  <button>Reset</button>
+);
+
+export default ResetList;
