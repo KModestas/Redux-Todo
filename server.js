5b4f9a5547360abbf6dc2d31e09da1ee3f0dbb5b
Modestas <modestask97@gmail.com>
2018-04-26 16:00:09 +0100

    added remove character reducer

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.listen(8000, ()=> console.log('server started'));
+mongoose.connect('mongodb://localhost:27017/redux-todo');
+
+app.use(bodyParser.json());
+
+app.get('/api/todos', (req, res) => {
+  res.json({ msg: 'sup '});
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'));
